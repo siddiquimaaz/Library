c72f7156cd197d625415132a553dfbcc79824092
MaazSiddiqui <siddiquimaaz88@gmail.com>
2024-05-03 20:41:10 +0500

    updated the funvtionlity and some errors are need to be fixed later for the data grid view and borrow book functiionality

--- AddBook.Designer.cs
+++ AddBook.Designer.cs
@@ -32,13 +32,14 @@
             label2 = new Label();
             label3 = new Label();
             booktitltxt = new TextBox();
-            booktitletxt = new TextBox();
+            authortxt = new TextBox();
             label4 = new Label();
-            addtobtn = new FontAwesome.Sharp.IconButton();
+            addToStdbtn = new FontAwesome.Sharp.IconButton();
             cancel = new FontAwesome.Sharp.IconButton();
             backbtn = new FontAwesome.Sharp.IconButton();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            booksView = new DataGridView();
+            SearchBtn = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)booksView).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -86,18 +87,18 @@
             booktitltxt.TabIndex = 3;
             booktitltxt.TextAlign = HorizontalAlignment.Center;
             // 
-            // booktitletxt
+            // authortxt
             // 
-            booktitletxt.BackColor = Color.White;
-            booktitletxt.BorderStyle = BorderStyle.FixedSingle;
-            booktitletxt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            booktitletxt.ForeColor = Color.Transparent;
-            booktitletxt.Location = new Point(119, 208);
-            booktitletxt.Name = "booktitletxt";
-            booktitletxt.PlaceholderText = "Enter Author name";
-            booktitletxt.Size = new Size(191, 27);
-            booktitletxt.TabIndex = 5;
-            booktitletxt.TextAlign = HorizontalAlignment.Center;
+            authortxt.BackColor = Color.White;
+            authortxt.BorderStyle = BorderStyle.FixedSingle;
+            authortxt.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            authortxt.ForeColor = Color.Transparent;
+            authortxt.Location = new Point(119, 208);
+            authortxt.Name = "authortxt";
+            authortxt.PlaceholderText = "Enter Author name";
+            authortxt.Size = new Size(191, 27);
+            authortxt.TabIndex = 5;
+            authortxt.TextAlign = HorizontalAlignment.Center;
             // 
             // label4
             // 
@@ -111,23 +112,24 @@
             label4.Text = "Author";
             label4.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // addtobtn
+            // addToStdbtn
             // 
-            addtobtn.BackColor = Color.Transparent;
-            addtobtn.FlatStyle = FlatStyle.Popup;
-            addtobtn.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            addtobtn.IconChar = FontAwesome.Sharp.IconChar.CheckCircle;
-            addtobtn.IconColor = Color.Black;
-            addtobtn.IconFont = FontAwesome.Sharp.IconFont.Regular;
-            addtobtn.IconSize = 28;
-            addtobtn.ImageAlign = ContentAlignment.MiddleLeft;
-            addtobtn.Location = new Point(119, 269);
-            addtobtn.Name = "addtobtn";
-            addtobtn.Size = new Size(72, 30);
-            addtobtn.TabIndex = 6;
-            addtobtn.Text = "Add";
-            addtobtn.TextAlign = ContentAlignment.MiddleRight;
-            addtobtn.UseVisualStyleBackColor = false;
+            addToStdbtn.BackColor = Color.Transparent;
+            addToStdbtn.FlatStyle = FlatStyle.Popup;
+            addToStdbtn.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            addToStdbtn.IconChar = FontAwesome.Sharp.IconChar.CheckCircle;
+            addToStdbtn.IconColor = Color.Black;
+            addToStdbtn.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            addToStdbtn.IconSize = 28;
+            addToStdbtn.ImageAlign = ContentAlignment.MiddleLeft;
+            addToStdbtn.Location = new Point(129, 312);
+            addToStdbtn.Name = "addToStdbtn";
+            addToStdbtn.Size = new Size(72, 30);
+            addToStdbtn.TabIndex = 6;
+            addToStdbtn.Text = "Add";
+            addToStdbtn.TextAlign = ContentAlignment.MiddleRight;
+            addToStdbtn.UseVisualStyleBackColor = false;
+            addToStdbtn.Click += addToStdbtn_Click;
             // 
             // cancel
             // 
@@ -138,13 +140,14 @@
             cancel.IconFont = FontAwesome.Sharp.IconFont.Auto;
             cancel.IconSize = 28;
             cancel.ImageAlign = ContentAlignment.MiddleLeft;
-            cancel.Location = new Point(197, 269);
+            cancel.Location = new Point(207, 312);
             cancel.Name = "cancel";
             cancel.Size = new Size(86, 30);
             cancel.TabIndex = 7;
             cancel.Text = "Cancel";
             cancel.TextAlign = ContentAlignment.MiddleRight;
             cancel.UseVisualStyleBackColor = true;
+            cancel.Click += cancel_Click;
             // 
             // backbtn
             // 
@@ -155,23 +158,44 @@
             backbtn.IconFont = FontAwesome.Sharp.IconFont.Solid;
             backbtn.IconSize = 28;
             backbtn.ImageAlign = ContentAlignment.MiddleLeft;
-            backbtn.Location = new Point(162, 320);
+            backbtn.Location = new Point(175, 358);
             backbtn.Name = "backbtn";
             backbtn.Size = new Size(76, 30);
             backbtn.TabIndex = 8;
             backbtn.Text = "Back";
             backbtn.TextAlign = ContentAlignment.MiddleRight;
             backbtn.UseVisualStyleBackColor = true;
+            backbtn.Click += backbtn_Click;
             // 
-            // dataGridView1
+            // booksView
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(552, 132);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(758, 564);
-            dataGridView1.TabIndex = 9;
+            booksView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            booksView.Location = new Point(552, 132);
+            booksView.Name = "booksView";
+            booksView.RowHeadersWidth = 51;
+            booksView.RowTemplate.Height = 29;
+            booksView.Size = new Size(758, 564);
+            booksView.TabIndex = 9;
+            // 
+            // SearchBtn
+            // 
+            SearchBtn.Cursor = Cursors.Hand;
+            SearchBtn.FlatStyle = FlatStyle.Popup;
+            SearchBtn.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            SearchBtn.IconChar = FontAwesome.Sharp.IconChar.MagnifyingGlass;
+            SearchBtn.IconColor = Color.Black;
+            SearchBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            SearchBtn.IconSize = 28;
+            SearchBtn.ImageAlign = ContentAlignment.MiddleLeft;
+            SearchBtn.Location = new Point(175, 259);
+            SearchBtn.Name = "SearchBtn";
+            SearchBtn.Padding = new Padding(1);
+            SearchBtn.Size = new Size(87, 34);
+            SearchBtn.TabIndex = 11;
+            SearchBtn.Text = "Search";
+            SearchBtn.TextAlign = ContentAlignment.MiddleRight;
+            SearchBtn.UseVisualStyleBackColor = true;
+            SearchBtn.Click += SearchBtn_Click;
             // 
             // AddBook
             // 
@@ -179,11 +203,12 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1314, 694);
-            Controls.Add(dataGridView1);
+            Controls.Add(SearchBtn);
+            Controls.Add(booksView);
             Controls.Add(backbtn);
             Controls.Add(cancel);
-            Controls.Add(addtobtn);
-            Controls.Add(booktitletxt);
+            Controls.Add(addToStdbtn);
+            Controls.Add(authortxt);
             Controls.Add(label4);
             Controls.Add(booktitltxt);
             Controls.Add(label3);
@@ -192,7 +217,7 @@
             Name = "AddBook";
             Text = "AddBook";
             Load += AddBook_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)booksView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -203,11 +228,12 @@
         private Label label2;
         private Label label3;
         private TextBox booktitltxt;
-        private TextBox booktitletxt;
+        private TextBox authortxt;
         private Label label4;
-        private FontAwesome.Sharp.IconButton addtobtn;
+        private FontAwesome.Sharp.IconButton addToStdbtn;
         private FontAwesome.Sharp.IconButton cancel;
         private FontAwesome.Sharp.IconButton backbtn;
-        private DataGridView dataGridView1;
+        private DataGridView booksView;
+        private FontAwesome.Sharp.IconButton SearchBtn;
     }
 }
