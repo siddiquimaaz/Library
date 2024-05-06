8e1dc0828c7327f900b2542ff9834576e1d2a463
MaazSiddiqui <siddiquimaaz88@gmail.com>
2024-05-06 19:36:51 +0500

    fixed some sizes from login form i.e: padding and image alignment

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -52,11 +52,11 @@
             text1.BorderStyle = BorderStyle.FixedSingle;
             text1.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             text1.ForeColor = Color.Black;
-            text1.Location = new Point(14, 259);
+            text1.Location = new Point(19, 259);
             text1.Margin = new Padding(3, 4, 3, 4);
             text1.Name = "text1";
             text1.PlaceholderText = "Enter username";
-            text1.Size = new Size(360, 23);
+            text1.Size = new Size(360, 27);
             text1.TabIndex = 0;
             text1.TextAlign = HorizontalAlignment.Center;
             // 
@@ -66,11 +66,11 @@
             text2.BorderStyle = BorderStyle.FixedSingle;
             text2.Font = new Font("Tahoma", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             text2.ForeColor = Color.Black;
-            text2.Location = new Point(14, 331);
+            text2.Location = new Point(19, 331);
             text2.Margin = new Padding(3, 4, 3, 4);
             text2.Name = "text2";
             text2.PlaceholderText = "Enter your Password";
-            text2.Size = new Size(360, 23);
+            text2.Size = new Size(360, 27);
             text2.TabIndex = 1;
             text2.TextAlign = HorizontalAlignment.Center;
             text2.UseSystemPasswordChar = true;
@@ -83,7 +83,7 @@
             userName.ForeColor = Color.Black;
             userName.Location = new Point(10, 236);
             userName.Name = "userName";
-            userName.Size = new Size(87, 18);
+            userName.Size = new Size(110, 23);
             userName.TabIndex = 2;
             userName.Text = "User-Name:";
             // 
@@ -95,7 +95,7 @@
             Pass.ForeColor = Color.Black;
             Pass.Location = new Point(10, 307);
             Pass.Name = "Pass";
-            Pass.Size = new Size(74, 18);
+            Pass.Size = new Size(95, 23);
             Pass.TabIndex = 3;
             Pass.Text = "Password:";
             // 
@@ -104,7 +104,7 @@
             Login.BackColor = Color.FromArgb(192, 192, 255);
             Login.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             Login.ForeColor = Color.White;
-            Login.Location = new Point(14, 388);
+            Login.Location = new Point(18, 388);
             Login.Margin = new Padding(3, 4, 3, 4);
             Login.Name = "Login";
             Login.Size = new Size(362, 48);
@@ -121,7 +121,7 @@
             hehe.ForeColor = Color.Black;
             hehe.Location = new Point(19, 476);
             hehe.Name = "hehe";
-            hehe.Size = new Size(202, 14);
+            hehe.Size = new Size(238, 18);
             hehe.TabIndex = 7;
             hehe.Text = "Don't have account? Register here:";
             // 
@@ -130,7 +130,7 @@
             sign.BackColor = Color.White;
             sign.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
             sign.ForeColor = Color.FromArgb(192, 192, 255);
-            sign.Location = new Point(14, 499);
+            sign.Location = new Point(21, 499);
             sign.Margin = new Padding(3, 4, 3, 4);
             sign.Name = "sign";
             sign.Size = new Size(357, 56);
@@ -148,7 +148,7 @@
             checkBox1.Location = new Point(14, 362);
             checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(112, 18);
+            checkBox1.Size = new Size(130, 22);
             checkBox1.TabIndex = 11;
             checkBox1.Text = "Show Password";
             checkBox1.TextAlign = ContentAlignment.MiddleCenter;
@@ -164,7 +164,7 @@
             checkadmin.Location = new Point(311, 362);
             checkadmin.Margin = new Padding(3, 2, 3, 2);
             checkadmin.Name = "checkadmin";
-            checkadmin.Size = new Size(60, 18);
+            checkadmin.Size = new Size(70, 22);
             checkadmin.TabIndex = 12;
             checkadmin.Text = "Admin";
             checkadmin.TextAlign = ContentAlignment.MiddleCenter;
@@ -178,6 +178,7 @@
             panel1.Location = new Point(0, 0);
             panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
+            panel1.Padding = new Padding(2);
             panel1.Size = new Size(387, 41);
             panel1.TabIndex = 13;
             // 
@@ -186,9 +187,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(359, 11);
+            label1.Location = new Point(361, 13);
             label1.Name = "label1";
-            label1.Size = new Size(15, 15);
+            label1.Size = new Size(19, 20);
             label1.TabIndex = 14;
             label1.Text = "X";
             label1.Click += label1_Click;
@@ -196,6 +197,7 @@
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Center;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(147, 58);
             pictureBox1.Margin = new Padding(3, 4, 3, 4);
@@ -209,15 +211,15 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Forte", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(146, 157);
+            label2.Location = new Point(135, 160);
             label2.Name = "label2";
-            label2.Size = new Size(104, 26);
+            label2.Size = new Size(129, 32);
             label2.TabIndex = 15;
             label2.Text = "Welcome";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 18F);
+            AutoScaleDimensions = new SizeF(10F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             BackgroundImageLayout = ImageLayout.Stretch;
