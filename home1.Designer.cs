0facc3f35d4c8358723b6907f497bec2d0529d91
MaazSiddiqui <siddiquimaaz88@gmail.com>
2024-06-02 21:11:57 +0500

    Made spacing even

--- home1.Designer.cs
+++ home1.Designer.cs
@@ -50,9 +50,10 @@
             addbk.FlatAppearance.BorderSize = 0;
             addbk.FlatStyle = FlatStyle.Popup;
             addbk.ForeColor = Color.Transparent;
-            addbk.Location = new Point(371, 145);
+            addbk.Location = new Point(424, 193);
+            addbk.Margin = new Padding(3, 4, 3, 4);
             addbk.Name = "addbk";
-            addbk.Size = new Size(117, 95);
+            addbk.Size = new Size(134, 127);
             addbk.TabIndex = 0;
             addbk.UseVisualStyleBackColor = false;
             addbk.Click += addbk_Click;
@@ -65,9 +66,10 @@
             button3.FlatAppearance.BorderSize = 0;
             button3.FlatStyle = FlatStyle.Popup;
             button3.ForeColor = Color.White;
-            button3.Location = new Point(558, 145);
+            button3.Location = new Point(629, 193);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(117, 95);
+            button3.Size = new Size(134, 127);
             button3.TabIndex = 2;
             button3.UseVisualStyleBackColor = false;
             // 
@@ -76,9 +78,9 @@
             ad.AutoSize = true;
             ad.BackColor = Color.Transparent;
             ad.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
-            ad.Location = new Point(389, 252);
+            ad.Location = new Point(445, 336);
             ad.Name = "ad";
-            ad.Size = new Size(76, 20);
+            ad.Size = new Size(95, 25);
             ad.TabIndex = 4;
             ad.Text = "Add Book";
             // 
@@ -87,9 +89,9 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI Semibold", 10.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(548, 252);
+            label1.Location = new Point(619, 336);
             label1.Name = "label1";
-            label1.Size = new Size(133, 19);
+            label1.Size = new Size(178, 25);
             label1.TabIndex = 6;
             label1.Text = "Renew Membership";
             // 
@@ -98,10 +100,9 @@
             HomeStdPic.BackColor = Color.Transparent;
             HomeStdPic.ErrorImage = (Image)resources.GetObject("HomeStdPic.ErrorImage");
             HomeStdPic.InitialImage = null;
-            HomeStdPic.Location = new Point(12, 22);
-            HomeStdPic.Margin = new Padding(3, 2, 3, 2);
+            HomeStdPic.Location = new Point(14, 29);
             HomeStdPic.Name = "HomeStdPic";
-            HomeStdPic.Size = new Size(89, 95);
+            HomeStdPic.Size = new Size(102, 127);
             HomeStdPic.SizeMode = PictureBoxSizeMode.Zoom;
             HomeStdPic.TabIndex = 8;
             HomeStdPic.TabStop = false;
@@ -111,9 +112,9 @@
             // HomeStdNameLabel
             // 
             HomeStdNameLabel.AutoSize = true;
-            HomeStdNameLabel.Location = new Point(18, 129);
+            HomeStdNameLabel.Location = new Point(21, 172);
             HomeStdNameLabel.Name = "HomeStdNameLabel";
-            HomeStdNameLabel.Size = new Size(0, 15);
+            HomeStdNameLabel.Size = new Size(0, 20);
             HomeStdNameLabel.TabIndex = 9;
             HomeStdNameLabel.Visible = false;
             // 
@@ -126,10 +127,10 @@
             logout.FlatStyle = FlatStyle.Popup;
             logout.ForeColor = Color.Black;
             logout.Image = (Image)resources.GetObject("logout.Image");
-            logout.Location = new Point(18, 485);
+            logout.Location = new Point(21, 647);
             logout.Margin = new Padding(0);
             logout.Name = "logout";
-            logout.Size = new Size(73, 38);
+            logout.Size = new Size(83, 51);
             logout.TabIndex = 10;
             logout.UseVisualStyleBackColor = false;
             logout.Click += logout_Click;
@@ -139,9 +140,9 @@
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(18, 518);
+            label3.Location = new Point(21, 691);
             label3.Name = "label3";
-            label3.Size = new Size(47, 15);
+            label3.Size = new Size(59, 20);
             label3.TabIndex = 11;
             label3.Text = "LogOut";
             // 
@@ -153,9 +154,10 @@
             DetailsBtn.FlatAppearance.BorderSize = 0;
             DetailsBtn.FlatStyle = FlatStyle.Popup;
             DetailsBtn.ForeColor = Color.White;
-            DetailsBtn.Location = new Point(730, 145);
+            DetailsBtn.Location = new Point(834, 193);
+            DetailsBtn.Margin = new Padding(3, 4, 3, 4);
             DetailsBtn.Name = "DetailsBtn";
-            DetailsBtn.Size = new Size(117, 95);
+            DetailsBtn.Size = new Size(134, 127);
             DetailsBtn.TabIndex = 12;
             DetailsBtn.UseVisualStyleBackColor = false;
             // 
@@ -164,20 +166,20 @@
             ReaderDetails.AutoSize = true;
             ReaderDetails.BackColor = Color.Transparent;
             ReaderDetails.Font = new Font("Segoe UI Semibold", 10.25F, FontStyle.Bold, GraphicsUnit.Point);
-            ReaderDetails.Location = new Point(767, 252);
+            ReaderDetails.Location = new Point(876, 336);
             ReaderDetails.Name = "ReaderDetails";
-            ReaderDetails.Size = new Size(52, 19);
+            ReaderDetails.Size = new Size(69, 25);
             ReaderDetails.TabIndex = 13;
             ReaderDetails.Text = "Details";
             // 
             // home
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightGray;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1199, 552);
+            ClientSize = new Size(1370, 736);
             Controls.Add(ReaderDetails);
             Controls.Add(DetailsBtn);
             Controls.Add(label3);
@@ -189,6 +191,7 @@
             Controls.Add(label1);
             Controls.Add(ad);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "home";
