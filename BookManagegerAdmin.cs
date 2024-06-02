d6e0b54e358f3f11f8415a8a020752f9e5cdb5ed
zeemen2724 <muhammadzainnaved@gmail.com>
2024-06-02 17:22:05 +0500

    test 2

--- BookManagegerAdmin.cs
+++ BookManagegerAdmin.cs
@@ -21,5 +21,15 @@ namespace Library
         {
 
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
