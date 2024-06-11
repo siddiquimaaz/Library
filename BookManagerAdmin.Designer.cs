6671d3d81b0ac759cfb9a4ad834185768180b4cd
MaazSiddiqui <siddiquimaaz88@gmail.com>
2024-06-11 18:15:14 +0500

    set size of add book admin

--- BookManagerAdmin.Designer.cs
+++ BookManagerAdmin.Designer.cs
@@ -68,9 +68,10 @@
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
-            panel1.Location = new Point(244, 79);
+            panel1.Location = new Point(198, 127);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(836, 399);
+            panel1.Size = new Size(955, 532);
             panel1.TabIndex = 0;
             panel1.Paint += panel1_Paint;
             // 
@@ -83,9 +84,10 @@
             BackBtn.IconChar = FontAwesome.Sharp.IconChar.None;
             BackBtn.IconColor = Color.Black;
             BackBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            BackBtn.Location = new Point(13, 362);
+            BackBtn.Location = new Point(15, 483);
+            BackBtn.Margin = new Padding(3, 4, 3, 4);
             BackBtn.Name = "BackBtn";
-            BackBtn.Size = new Size(75, 25);
+            BackBtn.Size = new Size(86, 33);
             BackBtn.TabIndex = 1;
             BackBtn.Text = "Back";
             BackBtn.UseVisualStyleBackColor = false;
@@ -95,9 +97,10 @@
             // 
             pictureBox1.BackgroundImageLayout = ImageLayout.Center;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(536, 88);
+            pictureBox1.Location = new Point(613, 117);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(223, 248);
+            pictureBox1.Size = new Size(255, 331);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 11;
             pictureBox1.TabStop = false;
@@ -107,9 +110,9 @@
             label6.AutoSize = true;
             label6.Font = new Font("Verdana", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(205, 31);
+            label6.Location = new Point(234, 41);
             label6.Name = "label6";
-            label6.Size = new Size(403, 35);
+            label6.Size = new Size(520, 45);
             label6.TabIndex = 5;
             label6.Text = "Add A Book in Database";
             // 
@@ -122,53 +125,59 @@
             submitBtn.IconChar = FontAwesome.Sharp.IconChar.Upload;
             submitBtn.IconColor = Color.White;
             submitBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            submitBtn.IconSize = 36;
+            submitBtn.IconSize = 32;
             submitBtn.ImageAlign = ContentAlignment.MiddleLeft;
-            submitBtn.Location = new Point(199, 290);
+            submitBtn.Location = new Point(227, 391);
+            submitBtn.Margin = new Padding(3, 4, 3, 4);
             submitBtn.Name = "submitBtn";
-            submitBtn.Size = new Size(167, 46);
+            submitBtn.Size = new Size(158, 46);
             submitBtn.TabIndex = 10;
             submitBtn.Text = "Submit";
             submitBtn.UseVisualStyleBackColor = false;
             // 
             // dateTimePicker
             // 
-            dateTimePicker.Location = new Point(199, 212);
+            dateTimePicker.Location = new Point(227, 283);
+            dateTimePicker.Margin = new Padding(3, 4, 3, 4);
             dateTimePicker.Name = "dateTimePicker";
-            dateTimePicker.Size = new Size(208, 23);
+            dateTimePicker.Size = new Size(237, 27);
             dateTimePicker.TabIndex = 9;
             // 
             // Genre
             // 
             Genre.ForeColor = Color.FromArgb(31, 43, 62);
-            Genre.Location = new Point(199, 239);
+            Genre.Location = new Point(227, 319);
+            Genre.Margin = new Padding(3, 4, 3, 4);
             Genre.Name = "Genre";
-            Genre.Size = new Size(150, 23);
+            Genre.Size = new Size(171, 27);
             Genre.TabIndex = 8;
             // 
             // ISBNNo
             // 
             ISBNNo.ForeColor = Color.FromArgb(31, 43, 62);
-            ISBNNo.Location = new Point(199, 183);
+            ISBNNo.Location = new Point(227, 244);
+            ISBNNo.Margin = new Padding(3, 4, 3, 4);
             ISBNNo.Name = "ISBNNo";
-            ISBNNo.Size = new Size(150, 23);
+            ISBNNo.Size = new Size(171, 27);
             ISBNNo.TabIndex = 7;
             // 
             // BookAuthor
             // 
             BookAuthor.ForeColor = Color.FromArgb(31, 43, 62);
-            BookAuthor.Location = new Point(199, 154);
+            BookAuthor.Location = new Point(227, 205);
+            BookAuthor.Margin = new Padding(3, 4, 3, 4);
             BookAuthor.Name = "BookAuthor";
-            BookAuthor.Size = new Size(150, 23);
+            BookAuthor.Size = new Size(171, 27);
             BookAuthor.TabIndex = 6;
             // 
             // BooKNameAdmin
             // 
             BooKNameAdmin.BorderStyle = BorderStyle.FixedSingle;
             BooKNameAdmin.ForeColor = Color.FromArgb(31, 43, 62);
-            BooKNameAdmin.Location = new Point(199, 126);
+            BooKNameAdmin.Location = new Point(227, 168);
+            BooKNameAdmin.Margin = new Padding(3, 4, 3, 4);
             BooKNameAdmin.Name = "BooKNameAdmin";
-            BooKNameAdmin.Size = new Size(150, 23);
+            BooKNameAdmin.Size = new Size(171, 27);
             BooKNameAdmin.TabIndex = 1;
             // 
             // label5
@@ -176,9 +185,9 @@
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(45, 212);
+            label5.Location = new Point(51, 283);
             label5.Name = "label5";
-            label5.Size = new Size(102, 17);
+            label5.Size = new Size(136, 23);
             label5.TabIndex = 4;
             label5.Text = "Publication Date";
             // 
@@ -187,9 +196,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(45, 241);
+            label4.Location = new Point(51, 321);
             label4.Name = "label4";
-            label4.Size = new Size(43, 17);
+            label4.Size = new Size(56, 23);
             label4.TabIndex = 3;
             label4.Text = "Genre";
             // 
@@ -198,9 +207,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(45, 189);
+            label3.Location = new Point(51, 252);
             label3.Name = "label3";
-            label3.Size = new Size(59, 17);
+            label3.Size = new Size(78, 23);
             label3.TabIndex = 2;
             label3.Text = "ISBN NO";
             // 
@@ -209,9 +218,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(45, 162);
+            label2.Location = new Point(51, 216);
             label2.Name = "label2";
-            label2.Size = new Size(80, 17);
+            label2.Size = new Size(106, 23);
             label2.TabIndex = 1;
             label2.Text = "Book Author";
             // 
@@ -220,9 +229,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(45, 134);
+            label1.Location = new Point(51, 179);
             label1.Name = "label1";
-            label1.Size = new Size(65, 17);
+            label1.Size = new Size(85, 23);
             label1.TabIndex = 0;
             label1.Text = "Book Title";
             // 
@@ -232,8 +241,9 @@
             panel2.Controls.Add(label7);
             panel2.Dock = DockStyle.Top;
             panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(1199, 36);
+            panel2.Size = new Size(1370, 48);
             panel2.TabIndex = 16;
             // 
             // label7
@@ -241,22 +251,21 @@
             label7.AutoSize = true;
             label7.Font = new Font("Verdana", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(3, 10);
+            label7.Location = new Point(3, 13);
             label7.Name = "label7";
-            label7.Size = new Size(473, 16);
+            label7.Size = new Size(617, 20);
             label7.TabIndex = 1;
             label7.Text = "Library Management System | Admin Panel | Book Management";
             // 
             // BookManagerAdmin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(15, 28, 46);
-            ClientSize = new Size(1199, 552);
+            ClientSize = new Size(1370, 710);
             Controls.Add(panel2);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "BookManagerAdmin";
